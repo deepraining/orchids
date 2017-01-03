b2bf4ae7fec777538d01e1c5074ba6f29860d5c0
senntyou <jiangjinbelief@163.com>
2017-01-03 21:37:54 +0800

    update

--- src/js/Page.js
+++ src/js/Page.js
@@ -10,9 +10,19 @@ var util = require('./util'),
      */
     defaultOption = {
         /**
+         * page name
+         */
+        pageName: '',
+        /**
          * background of root element
          */
         backgroundColor: defaultBackgroundColor,
+        /**
+         * start page with route or not
+         * if set to true, the url will be changed when a page is started
+         * like: url?orchidsPage=pageName&orchidsOption=serializedData
+         */
+        route: !1,
         /**
          * whether to use animation when switch between pages
          * default: true
@@ -22,90 +32,204 @@ var util = require('./util'),
          * animation direction of switching page
          * horizontal/vertical, default: horizontal
          */
-        animateDirection: 'horizontal'
-
+        animateDirection: 'horizontal',
+        /**
+         * whether current page is singleton or not.
+         * if set to true, it will not be removed from dom when called onDestroy, and will not render again when it is initialized again
+         */
+        singleton: !1
     };
 
-function Page(option) {
-    var self = this;
-    self.option = util.extend(true, {}, defaultOption, option);
-    self.__orchids__preInit();
-    self.__orchids__init();
-
-}
-
-Page.prototype = {
-    __orchids__preInit: function () {
-        var self = this,
-            color = self.option.backgroundColor;
-        // have color attribute
-        if (!!color) {
-            !/^#\w{6}$/.test(color) && !/^\w{6}$/.test(color) ? (
-                self.backgroundColor = '',
-                console.error('The backgroundColor attribute of Page option could not be parsed with wrong format.\nthe suggested format is: #000000~#ffffff/000000~ffffff, and input is: "' + color + '"')
-            ) : (
-                color.slice(0, 1) != '#' && (self.option.backgroundColor = '#' + color)
-            )
-        }
-        // no
-        else {
-            self.option.backgroundColor = '';
-        }
-    },
-    __orchids__init: function() {
-        var self = this,
-            classes = [
-                'orchids',
-                'orchids-page'
-            ];
-        // make id
-        self.id = idGenerator.getPageId();
-        // whether current page is the first page to render or not, for confirming to start current page with or without animation.
-        self.__orchids__isFirstPage = self.id == 1;
-        // make root el
-        self.el = document.createElement('div');
-        // animation
-        self.option.animate && (
-            classes.push('orchids-with-animation'),
-                self.option.animateDirection == 'vertical' ? classes.push('orchids-vertical') : classes.push('orchids-horizontal')
-        );
-        self.__orchids__isFirstPage && classes.push('orchids-active');
-        self.el.classList = classes.join(' ');
-        // background color
-        self.el.style.backgroundColor = !!self.option.backgroundColor ? self.option.backgroundColor : (
-            !!app.option.backgroundColor ? app.option.backgroundColor : defaultBackgroundColor
-        );
-
-        // user custom initialization
-        !!self.onCreate && self.onCreate();
-
-        // add to body element
-        document.body.appendChild(el);
-
-        // show page
-        !self.__orchids__isFirstPage && setTimeout(function () {
-            self.el.classList += ' orchids-active'
-        }, 0);
-    },
-
-    // hide current page
-    __orchids__hide: function () {
-
-    },
-
-    // render a page after a page is initialized
-    onCreate: function() {},
-
-    // pre handle before destroy a page
-    onDestroy: function() {},
-
+var newPage = function () {
     /**
-     * set the result if this page is called by startPageForResult method,
-     * and the returned value will be used as the param of the onPageResult method of last page
      *
-     * @param {*} data
+     * @param option Option to initialize page
+     * @param forResult Whether current page is called by startPage or startPageForResult
+     * @constructor
      */
-    setResult: function(data) {
-
+    function Page(option, forResult) {
+        var self = this;
+        self.option = util.extend(true, {}, option);
+        self.__orchids__preInit();
+        self.__orchids__init();
     }
+
+    Page.prototype = {
+        constructor: Page,
+        __orchids__preInit: function () {
+            var self = this,
+                color = self.option.backgroundColor;
+            // have color attribute
+            if (!!color) {
+                !/^#\w{6}$/.test(color) && !/^\w{6}$/.test(color) ? (
+                    self.backgroundColor = '',
+                        console.error('The backgroundColor attribute of Page option could not be parsed with wrong format.\nthe suggested format is: #000000~#ffffff/000000~ffffff, and input is: "' + color + '"')
+                ) : (
+                    color.slice(0, 1) != '#' && (self.option.backgroundColor = '#' + color)
+                )
+            }
+            // no
+            else {
+                self.option.backgroundColor = '';
+            }
+        },
+        __orchids__init: function() {
+            var self = this,
+                classes = [
+                    'orchids',
+                    'orchids-page'
+                ];
+            // make id
+            self.id = idGenerator.getPageId();
+            // whether current page is the first page to render or not, for confirming to start current page with or without animation.
+            self.__orchids__isFirstPage = self.id == 1;
+            // make root el
+            self.el = document.createElement('div');
+            // data-orchids-page-is
+            self.el.dataset.orchidsPageId = self.id;
+            // animation
+            // self
+            if (typeof self.option.animate != 'undefined') {
+                !!self.option.animate && (
+                    classes.push('orchids-with-animation'),
+                        self.option.animateDirection == 'vertical' ? classes.push('orchids-vertical') : classes.push('orchids-horizontal')
+                )
+            }
+            // app
+            else if (typeof app.option.animate != 'undefined') {
+                !!app.option.animate && (
+                    classes.push('orchids-with-animation'),
+                        app.option.animateDirection == 'vertical' ? classes.push('orchids-vertical') : classes.push('orchids-horizontal')
+                )
+            }
+            // default
+            else {
+                classes.push('orchids-with-animation'),
+                    classes.push('orchids-horizontal')
+            }
+            // classList
+            self.__orchids__isFirstPage && classes.push('orchids-active');
+            self.el.classList = classes.join(' ');
+            // background color
+            self.el.style.backgroundColor = !!self.option.backgroundColor ? self.option.backgroundColor : (
+                !!app.option.backgroundColor ? app.option.backgroundColor : defaultBackgroundColor
+            );
+            // z-index
+            !!self.option.singleton && (self.el.style.zIndex = 2);
+            // user custom initialization
+            !!self.onCreate && self.onCreate();
+
+            // route
+            // self
+            if (typeof self.option.route != 'undefined') {
+                !!self.option.route && self.__orchids__routeForward();
+            }
+            // app
+            else if (typeof app.option.route != 'undefined') {
+                !!app.option.route && self.__orchids__routeForward()
+            }
+
+            // add to body element
+            document.body.appendChild(el);
+
+            // show page
+            !self.__orchids__isFirstPage && setTimeout(function () {
+                self.el.classList.add('orchids-active')
+            }, 0);
+        },
+        // make a forward route
+        __orchids__routeForward: function () {
+            var self = this,
+                pageName = encodeURIComponent(self.option.pageName),
+                pageOption = encodeURIComponent(JSON.stringify(self.option)),
+                params = (function () {
+                    var params = {};
+                    !!location.search && (
+                        location.search.slice(1).split('&').map(function (item) {
+                            params[item.split('=')[0]] = item.split('=')[1]
+                        })
+                    );
+                    return params;
+                })(),
+                searchString = '';
+            params.orchidsPage = pageName;
+            params.orchidsOption = pageOption;
+
+            Object.keys(params).map(function (key) {
+                searchString += '&' + key + '=' + (!!params[key] ? params[key] : '');
+            });
+
+            history.pushState(null, null, '?' + searchString.slice(1));
+        },
+        // back a route
+        __orchids__routeBack: function () {
+            history.back();
+        },
+        // hide current page
+        __orchids__hide: function () {
+            var self = this,
+                prevPage = app.getPrevPage();
+            self.onDestroy();
+            !!prevPage.onPageResult && prevPage.onPageResult(self.__orchids__result);
+            self.el.classList.remove('orchids-active');
+            !self.option.singleton && setTimeout(function () {
+                self.el.remove()
+            }, 500);
+            // route
+            // self
+            if (typeof self.option.route != 'undefined') {
+                !!self.option.route && self.__orchids__routeBack();
+            }
+            // app
+            else if (typeof app.option.route != 'undefined') {
+                !!app.option.route && self.__orchids__routeBack()
+            }
+        },
+
+        // show current page
+        __orchids__show: function () {
+            var self = this;
+            // route
+            // self
+            if (typeof self.option.route != 'undefined') {
+                !!self.option.route && self.__orchids__routeForward();
+            }
+            // app
+            else if (typeof app.option.route != 'undefined') {
+                !!app.option.route && self.__orchids__routeForward()
+            }
+            self.el.classList.add('orchids-active');
+        },
+
+        // render a page after a page is initialized
+        onCreate: function() {},
+
+        // pre handle before destroy a page
+        onDestroy: function() {},
+
+        /**
+         * set the result if this page is called by startPageForResult method,
+         * and the returned value will be used as the param of the onPageResult method of last page
+         *
+         * @param {*} data
+         */
+        setResult: function(data) {
+            var self = this;
+            self.__orchids__result = data;
+        },
+        /**
+         * called when the child page destroyed and return the value by setResult method.
+         * @param {*} data
+         */
+        onPageResult: function(data) {},
+        /**
+         * receive data from the previous page, startPageForResult method's second parameter
+         * @param data
+         */
+        prepareForResult: function(data) {}
+    };
+
+    return Page;
 };
+
+module.exports = newPage;
--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,12 @@ var util = require('./util'),
          */
         backgroundColor: '#ffffff',
         /**
+         * start page with route or not
+         * if set to true, the url will be changed when a page is started
+         * like: url?orchidsPage=pageName&orchidsOption=serializedData
+         */
+        route: !1,
+        /**
          * whether to use animation when switch between pages
          * default: true
          */
@@ -30,9 +36,98 @@ var util = require('./util'),
 
     };
 
-var app = {
+/**
+ *
+ * @param option Option to initialize App
+ * @constructor
+ */
+function App(option) {
+    var self = this;
+    // global option for all pages
+    self.option = util.extend(true, {}, option);
+    self.__orchids__preInit();
+    self.__orchids__init();
+}
+
+App.prototype = {
+    constructor: App,
+    // current page id, index from 1
     currentPageId: 0,
-    initialize: function(option) {
-        app.option = util.extend(true, {}, defaultOption, option);
+    // current result for onPageResult and seResult method
+    currentResult: void 0,
+    /**
+     * all page instances container
+     * format: {
+         *     pageId: {
+         *         name: 'name',
+         *         singleton: true/false,
+         *         instance: instanceOfPage
+         *     }
+         * }
+     * @type {{}}
+     */
+    pages: {},
+
+    __orchids__preInit: function () {
+        var self = this,
+            color = self.option.backgroundColor;
+        // have color attribute
+        if (!!color) {
+            !/^#\w{6}$/.test(color) && !/^\w{6}$/.test(color) ? (
+                self.backgroundColor = '',
+                    console.error('The backgroundColor attribute of App option could not be parsed with wrong format.\nthe suggested format is: #000000~#ffffff/000000~ffffff, and input is: "' + color + '"')
+            ) : (
+                color.slice(0, 1) != '#' && (self.option.backgroundColor = '#' + color)
+            )
+        }
+        // no
+        else {
+            self.option.backgroundColor = '';
+        }
+    },
+    __orchids__init: function() {
+        var self = this,
+            params = (function () {
+                var params = {};
+                !!location.search && (
+                    location.search.slice(1).split('&').map(function (item) {
+                        params[item.split('=')[0]] = item.split('=')[1]
+                    })
+                );
+                return params;
+            })(),
+            pageName,
+            pageOption;
+
+        // tell the first page and option by the parameter
+        if (!!params.orchidsPage) {
+            pageName = decodeURIComponent(params.orchidsPage);
+            try {
+                pageOption = JSON.parse(decodeURIComponent(params.orchidsOption));
+            } catch (e) {
+                pageOption = {};
+            }
+
+            self.startPage(pageName, pageOption);
+        }
+    },
+    /**
+     * initialize a Page and show it
+     * @param pageName The name of a Page Object
+     * @param option Option to initialize a Page
+     */
+    startPage: function (pageName, option) {
+
+    },
+    /**
+     * initialize a Page and show it, with a returned value when the started page is destroyed
+     * @param pageName The name of a Page Object
+     * @param option Option to initialize a Page
+     * @param data Parameter of the next Page's prepareForResult method
+     */
+    startPageForResult: function (pageName, option, data) {
+
     }
 };
+
+module.exports = App;
